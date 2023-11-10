edc76f4eddfbde0ec794dfbd72d54e04d0d86f8b
CalebBoluwade <87561940+CalebBoluwade@users.noreply.github.com>
2023-11-10 11:43:42 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 
 npm install ts-node tsc typescript
 
+npm i -g pm2
+
 npm run build:dev
 
-node DIST/index.js
+pm2 start DIST/index.js --name BRIDGE_V1 -i max -o ./LOGS/SERVER.log -e ./LOGS/error/error.log -f
