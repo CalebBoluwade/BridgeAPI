f3fe19ce36383b72b9caf1122b5858d5e5e3db2e
CalebBoluwade <87561940+CalebBoluwade@users.noreply.github.com>
2024-08-31 11:46:35 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,18 @@
 set -e
 
 # /root/profile
-# set -o allexport; source ./.env; set +o allexport
+# set -o allexport; source .env; set +o allexport
 
-npm install -g pm2 ts-node typescript nodemon
+npm install -g pm2 ts-node typescript
+    
+# netstat -plant
 
-npm install
 # which node # which docker
 
 # printenv
 
-# pwd && ls
+pwd && ls
 
-# echo -e "------ STARTING APPLICATION -------" | tsc -p . && pm2 start ecosystem.config.js
+# echo -e "--------------- STARTING APPLICATION --------------" | pm2 start ecosystem.config.js
 
-echo -e "--------------- STARTING APPLICATION --------------" | npm run prod
+echo -e "--------------- STARTING APPLICATION --------------" | npm run dev
